93fba1a71850ca38233740068fd43691756584b9
Marcos dos Santos Luiz <marcodossantosluiz111@gmail.com>
2022-04-13 22:24:59 -0300

    1.0.0

---
+++ atividade2/modulos/funcoes.js
@@ -0,0 +1,10 @@
+//FUNÇÃO CALCULAR IDADE E IMC
+
+module.exports.calcularIdade = function (anoNasc) {
+    return 2022 - anoNasc;
+}
+
+module.exports.calcularImc = function (peso, altura) {
+    return (peso / altura ** 2);
+}
+
